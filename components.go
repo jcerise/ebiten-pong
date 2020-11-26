b19bc50070c320807a833fc4e27eb3f4ca7fcf6b
jcerise <jcerise06@gmail.com>
2020-11-25 17:51:41 -0700

    Added a properly bouncing ball, and basic collision detection with the ball against the paddles (checks for rectangle overlaps).

--- components.go
+++ components.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/hajimehoshi/ebiten/v2"
+
 type Position struct {
 	X float64
 	Y float64
@@ -8,4 +10,19 @@ type Position struct {
 type Velocity struct {
 	vx float64
 	vy float64
+}
+
+type Paddle struct {
+	image *ebiten.Image
+	position Position
+	width int
+	height int
+}
+
+type Ball struct {
+	image *ebiten.Image
+	position Position
+	width int
+	height int
+	velocity Velocity
 }
--- main.go
+++ main.go
@@ -11,14 +11,13 @@ const (
 	SCREENWIDTH = 640
 	SCREENHEIGHT = 480
 
-	BALLSPEED = 3
+	BALLSPEED = 4
 )
 
 var (
 	// Player variables
 	playerImage *ebiten.Image
-	playerHeight int
-	playerPos Position
+	player Paddle
 
 	// Opponent variables
 	opponentImage *ebiten.Image
@@ -28,10 +27,7 @@ var (
 
 	// Ball variables
 	ballImage *ebiten.Image
-	ballWidth int
-	ballHeight int
-	ballPos Position
-	ballVelocity Velocity
+	ball Ball
 
 )
 
@@ -57,70 +53,77 @@ func init() {
 
 	// Set the starting positions for the player (Left side of screen), the opponent (right side of screen), and the
 	// ball (center of screen)
-	playerPos = Position{0, 0}
-	_, playerHeight = playerImage.Size()
+	playerPos := Position{0, 0}
+	playerWidth, playerHeight := playerImage.Size()
+	player = Paddle{playerImage, playerPos, playerWidth, playerHeight}
 
 	opponentWidth, opponentHeight  = opponentImage.Size()
 	opponentPos = Position{float64(SCREENWIDTH - opponentWidth), 0}
 
-	ballWidth, ballHeight = ballImage.Size()
-	ballPos = Position{float64((SCREENWIDTH / 2) - ballWidth), float64((SCREENHEIGHT / 2) - ballHeight)}
-	ballVelocity = Velocity{
-		2,
+	ballWidth, ballHeight := ballImage.Size()
+	ballPos := Position{float64((SCREENWIDTH / 2) - ballWidth), float64((SCREENHEIGHT / 2) - ballHeight)}
+	ballVelocity := Velocity{
 		4,
+		2,
 	}
+	ball = Ball{ballImage, ballPos, ballWidth, ballHeight, ballVelocity}
 }
 
 type Game struct{}
 
 func (g *Game) Update() error {
+
+	var movingUp = false
+	var movingDown = false
+
 	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		if playerPos.Y >= float64(SCREENHEIGHT - playerHeight) {
-			playerPos.Y = float64(SCREENHEIGHT - playerHeight)
+		if player.position.Y >= float64(SCREENHEIGHT - player.height) {
+			player.position.Y = float64(SCREENHEIGHT - player.height)
 		} else {
-			playerPos.Y += 4
+			player.position.Y += 4
+			movingDown = true
 		}
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		if playerPos.Y <= 0 {
-			playerPos.Y = 0
+		if player.position.Y <= 0 {
+			player.position.Y = 0
 		} else {
-			playerPos.Y -= 4
+			player.position.Y -= 4
+			movingUp = true
 		}
 	}
 
 	// If the ball has reached an edge, calculate a new velocity for it based on where it hit
 	//var bounceAngle float64
-	if ballPos.Y <= 0 || (ballPos.Y + float64(ballHeight)) >= SCREENHEIGHT {
-		//var relativeIntersectX = (SCREENWIDTH / 2) - ballPos.X
-		//var normalizedIntesectX = relativeIntersectX / (SCREENWIDTH / 2)
-		//bounceAngle = normalizedIntesectX * 75
-		//
-		//ballVelocity.vx = BALLSPEED * math.Cos(bounceAngle)
-		//ballVelocity.vy = BALLSPEED * -math.Sin(bounceAngle)
-		ballVelocity.vy = -ballVelocity.vy
+	var newVelocity Velocity
+	if ball.position.Y <= 0 || (ball.position.Y + float64(ball.height)) >= SCREENHEIGHT {
+		newVelocity = Velocity{ball.velocity.vx, -ball.velocity.vy}
+		ball.velocity = newVelocity
+	} else if (ball.position.X + float64(ball.height)) >= SCREENWIDTH {
+		newVelocity = Velocity{-ball.velocity.vx, ball.velocity.vy}
+		ball.velocity = newVelocity
+	} else if checkBallCollision(player, ball) {
+		newVelocity = Velocity{-ball.velocity.vx, ball.velocity.vy}
+
+		if movingUp {
+			newVelocity.vy -= 2
+		} else if movingDown {
+			newVelocity.vy += 2
+		}
+
+		ball.velocity = newVelocity
 	}
 
-	if ballPos.X <= 0 || ballPos.X + float64(ballWidth) >= SCREENWIDTH {
-		//var relativeIntersectY = (SCREENHEIGHT / 2) - ballPos.Y
-		//var normalizedIntesectY = relativeIntersectY / (SCREENHEIGHT / 2)
-		//bounceAngle = normalizedIntesectY * 75
-		//
-		//ballVelocity.vx = BALLSPEED * -math.Cos(bounceAngle)
-		//ballVelocity.vy = BALLSPEED * math.Sin(bounceAngle)
-		ballVelocity.vx = -ballVelocity.vx
-	}
-
-	ballPos.X += ballVelocity.vx
-	ballPos.Y += ballVelocity.vy
+	ball.position.X += ball.velocity.vx
+	ball.position.Y += ball.velocity.vy
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	playerOptions := &ebiten.DrawImageOptions{}
-	playerOptions.GeoM.Translate(playerPos.X, playerPos.Y)
+	playerOptions.GeoM.Translate(player.position.X, player.position.Y)
 	screen.DrawImage(playerImage, playerOptions)
 
 	opponentOptions := &ebiten.DrawImageOptions{}
@@ -128,8 +131,23 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(opponentImage, opponentOptions)
 
 	ballOptions := &ebiten.DrawImageOptions{}
-	ballOptions.GeoM.Translate(ballPos.X, ballPos.Y)
+	ballOptions.GeoM.Translate(ball.position.X, ball.position.Y)
 	screen.DrawImage(ballImage, ballOptions)
+}
+
+// checkBallCollision checks if a ball struct is currently colliding with a paddle struct. This is accomplished by
+// taking the rectangles around each struct, and checking if there is a gap between any of the four sides of the
+// bounding rectangle created by the paddle and ball. If a gap is found, no collision is occurring.
+func checkBallCollision(paddle Paddle, ball Ball) bool {
+	if paddle.position.X < (ball.position.X + float64(ball.width)) &&
+		(paddle.position.X + float64(paddle.width)) > ball.position.X &&
+		paddle.position.Y < (ball.position.Y + float64(ball.height)) &&
+		(paddle.position.Y + float64(paddle.height)) > ball.position.Y {
+		// A collision has been detected between the paddle and ball
+		return true
+	}
+
+	return false
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
